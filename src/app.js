f6d2a860aa06d57ab9aa0ff48635c13b88a7d2a0
taufik69 <taufik35-2915@diu.edu.bd>
2024-06-26 17:49:50 +0600

    add middleware

---
+++ src/app.js
@@ -0,0 +1,29 @@
+import express from "express";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+const app = express();
+/**
+ * todo : All middleware
+ * *motive: Middleware are used to configuration
+ */
+app.use(
+  cors({
+    origin: process.env.CORS_ORIGIN,
+    credentials: true,
+  })
+);
+
+/**
+ * Todo : This middleware express.json are used to parse from data
+ */
+app.use(express.json({ limit: process.env.REQUESTED_DATA_SIZE }));
+/**
+ * Todo : This middleware are parse search string and like extract data from url
+ */
+app.use(
+  express.urlencoded({ extended: true, limit: process.env.REQUESTED_DATA_SIZE })
+);
+app.use(express.static("public"));
+app.use(cookieParser());
+
+export { app };
--- src/index.js
+++ src/index.js
@@ -1,6 +1,15 @@
 // require("dotenv").config({ path: "/.env" });
 import dotenv from "dotenv";
 import DBConnection from "./Database/index.js";
+import { app } from "./app.js";
 dotenv.config({ path: "/.env" });
 
-DBConnection();
+DBConnection()
+  .then(() => {
+    app.listen(process.env.PORT || 5000, () => {
+      console.log(`Server Running on Port ${process.env.PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.log(`MONGODB CONNECTION  ERROR !!! ${err}`);
+  });
