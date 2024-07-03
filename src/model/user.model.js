0a0ef2c73ebcea2e5e10b229e04a695f6306599e
taufik69 <taufik35-2915@diu.edu.bd>
2024-07-03 17:41:15 +0600

    model and utils

---
+++ src/model/user.model.js
@@ -0,0 +1,41 @@
+import mongoose, { Schema } from "mongoose";
+
+const userSchema = new Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+      index: true,
+      lowercase: true,
+      trim: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+      trim: true,
+    },
+    fullName: {
+      type: String,
+      required: true,
+      trim: true,
+      index: true,
+    },
+    avatar: {
+      type: String,
+      required: true,
+    },
+    coverImage: {
+      type: String,
+    },
+    watchHistory: {
+      type: Schema.Types.ObjectId,
+      ref: "Vedio",
+    },
+  },
+  { timestamps: true }
+);
+
+export const model = mongoose.model("User", userSchema);
---
+++ src/utils/ApiError.js
@@ -0,0 +1,13 @@
+class ApiError extends Error {
+  constructor(
+    statusCode,
+    message = "Somethings Went Wrong!!",
+    errors = [],
+    stack = ""
+  ) {
+    super(message);
+    this.statusCode = statusCode;
+    (this.data = null), (this.message = message), (this.success = false);
+    this.errors = errors;
+  }
+}
